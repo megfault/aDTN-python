818c0b9a1109e90c82df3976377b2218e28abfa9
megfault <ana@megfau.lt>
2016-04-13 14:19:54 +0200

    add logging config

--- aDTN.py
+++ aDTN.py
@@ -20,6 +20,9 @@ DATABASE_FN = "messagestore.db"
 PACKET_SIZE = 1500
 MAX_INNER_SIZE = 1466
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(levelname)s] %(message)s',
+                    )
 
 def generate_iv():
     return rand(SecretBox.NONCE_SIZE)
