de5404fbdbff14ae2b23fff7846354d541907392
megfault <ana@megfau.lt>
2016-09-05 19:47:16 +0200

    remove tests to deleted function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,17 +3,6 @@ from nacl.utils import random
 from nacl.secret import SecretBox
 
 
-def test_randomness():
-    rand1 = generate_iv()
-    rand2 = generate_iv()
-    assert rand1 != rand2
-
-
-def test_iv_length():
-    rand = generate_iv()
-    assert len(rand) == SecretBox.NONCE_SIZE
-
-
 def test_Xcryption():
     msg = b'hello'
     key = random(SecretBox.KEY_SIZE)
